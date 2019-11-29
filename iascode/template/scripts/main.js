824d96f4203d7299847bfa973a03e355c78b2658
Gustavo Andres Brey <gusbrey@gmail.com>
2019-11-29 11:13:16 -0300

    Fecha

--- iascode/template/scripts/main.js
+++ iascode/template/scripts/main.js
@@ -5,7 +5,7 @@ $(function() {
   animateNav();
 
   new WOW().init();
-  setupCountdown('2019/10/24 16:00:00', ['dias', 'horas', 'minutos', 'segundos'], $('#countdown-template'), $('#countdown-container'));
+  setupCountdown('2019/12/03 16:00:00', ['dias', 'horas', 'minutos', 'segundos'], $('#countdown-template'), $('#countdown-container'));
 });
 
 function configureScroll () {
